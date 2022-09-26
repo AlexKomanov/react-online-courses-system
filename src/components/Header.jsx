0682ac3ba5f2629a6f0fb2522b12dd3cc5bef5d1
AlexKomanov <akomanov88@gmail.com>
2022-09-26 13:39:07 +0300

    Refactoring of the Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,54 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { FaFacebook, FaGithub, FaLinkedin, FaYoutube } from 'react-icons/fa'
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link'
+import IconButton from '@mui/material/IconButton';
+import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
 
 const Header = () => {
     return (
-        <header className='header-container'>
-            <NavLink to="/" >
-                {/* <img src={logo} alt="logo" height="140$"/> */}
-                <div className='header-container-logo'>
-                    <p>Geek of Automation</p>
-                </div>
-            </NavLink>
-            <div className='header-container-right'>
-                <a href="https://github.com/AlexKomanov/react-online-courses-system" target="_blank">
-                    <FaGithub />
-                </a>
-                <a href="https://www.youtube.com/channel/UCtkgFDoX3_VaJJ9Aj0EEZxg" target="_blank">
-                    <FaYoutube />
-                </a>
-                <a href="https://www.linkedin.com/in/alexander-komanov-88b138158/" target="_blank">
-                    <FaLinkedin />
-                </a>
-                <a href="https://www.facebook.com/akomanov" target="_blank">
-                    <FaFacebook />
-                </a>
+        <Box sx={{ flexGrow: 1 }} >
+            <AppBar position="static" color='primary' >
+
+                <Toolbar>
+                    <ComputerOutlinedIcon size="large" color='inherit' sx={{ mr: 2 }} />
+
+                    <Typography variant="h6" noWrap component="div"
+                        sx={{
+                            mr: 2, fontFamily: 'serif', fontSize: 23,
+                            fontWeight: 600, letterSpacing: '.2rem',
+                            flexGrow: 1, color: 'inherit',
+                        }} >
+                        <Link href="/" underline="none" color="inherit">
+                            Geek of Automation
+                        </Link>
+                    </Typography>
+
+                    <IconButton color='inherit' size="large" sx={{ mr: 0.5 }} href="https://github.com/AlexKomanov/react-online-courses-system" target="_blank">
+                        <GitHubIcon />
+                    </IconButton>
+                    <IconButton color='inherit' size="large" sx={{ mr: 0.5 }} href="https://www.youtube.com/channel/UCtkgFDoX3_VaJJ9Aj0EEZxg" target="_blank">
+                        <YouTubeIcon />
+                    </IconButton>
+                    <IconButton color='inherit' size="large" sx={{ mr: 0.5 }} href="https://www.facebook.com/akomanov" target="_blank">
+                        <FacebookOutlinedIcon />
+                    </IconButton>
+                    <IconButton color='inherit' size="large" sx={{ mr: 0.5 }} href="https://www.linkedin.com/in/alexander-komanov-88b138158/" target="_blank">
+                        <LinkedInIcon />
+                    </IconButton>
 
 
-            </div>
-        </header>
+                </Toolbar>
+            </AppBar>
+        </Box>
+
     )
 }
 
