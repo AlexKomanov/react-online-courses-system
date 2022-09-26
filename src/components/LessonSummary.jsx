36800d4182946252e649851f9388f4d820af1a3c
AlexKomanov <akomanov88@gmail.com>
2022-09-26 20:58:10 +0300

    Rolback with the changes

--- src/components/LessonSummary.jsx
+++ src/components/LessonSummary.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from '@mui/material/Link'
+import { Link } from 'react-router-dom'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -8,30 +8,22 @@ import CardMedia from '@mui/material/CardMedia';
 
 const LessonSummary = (props) => {
     return (
-        <section key={props.lesson.id} >
-             <Card sx={{ maxWidth: 500, m: 'auto', mt: 3, mb: 3}}>
-            <CardMedia
-                component="img"
-                height="100"
-                image="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210210175213/JavaScriptTutorial.png"
-            />
-            <CardContent >
-
-                <Typography gutterBottom variant="h5" component="div" fontFamily="serif" fontWeight="600">
-                    <Link href={`/courses/${props.courseId}/lessons/${props.lesson.id}`} underline="none" color="inherit">
-                    {props.lesson.id}. {props.lesson.title}
+        <section key={props.lesson.id} className="summary">
+            <div>
+                <div className='title'>
+                    <h2>
+                        <Link className="no-underline cursor-pointer" to={`/courses/${props.courseId}/lessons/${props.lesson.id}`}>
+                            {props.lesson.id}. {props.lesson.title}
+                        </Link>
+                    </h2>
+                </div>
+                <p>
+                    <Link className="no-underline cursor-pointer" to={`/courses/${props.courseId}/lessons/${props.lesson.id}`}>
+                        {props.lesson.description}
                     </Link>
-                </Typography>
+                </p>
+            </div>
 
-                <Typography variant="h6" color="text.secondary" component="div" fontFamily="serif" >
-                    <Link href={`/courses/${props.courseId}/lessons/${props.lesson.id}`} underline="none" color="inherit">
-                    {props.lesson.description}
-                    </Link>
-                </Typography>
-            </CardContent>
-
-        </Card>
- 
         </section>
     )
 }
