5abdd7a97facf7328ce03184de494825041434ac
AlexKomanov <akomanov88@gmail.com>
2022-09-26 21:32:58 +0300

    Changing to the react-dom-router link

--- src/components/CourseSummary.jsx
+++ src/components/CourseSummary.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Link from '@mui/material/Link'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
+import { Link } from 'react-router-dom'
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
@@ -10,21 +10,21 @@ const CourseSummary = (props) => {
     return (
         <>
         <Card sx={{ maxWidth: 600, m: 'auto', mt: 2 }}>
-            {/* <CardMedia
+            <CardMedia
                 component="img"
                 height="140"
                 image="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210210175213/JavaScriptTutorial.png"
-            /> */}
+            />
             <CardContent >
 
                 <Typography gutterBottom variant="h5" component="div" fontFamily="serif" fontWeight="600">
-                    <Link href={`/courses/${props.course.slug}`} underline="none" color="inherit">
+                    <Link to={`/courses/${props.course.slug}`} underline="none" color="inherit">
                         {props.course.title}
                     </Link>
                 </Typography>
 
                 <Typography variant="h6" color="text.secondary" component="div" fontFamily="serif" >
-                    <Link href={`/courses/${props.course.slug}`} underline="none" color="inherit">
+                    <Link to={`/courses/${props.course.slug}`} underline="none" color="inherit">
                         {props.course.description}
                     </Link>
                 </Typography>
