4612e61858a74513e731f0b97439d6a7e8d38343
AlexKomanov <akomanov88@gmail.com>
2022-09-26 16:51:30 +0300

    Restyling of Courses Page

--- src/components/CourseSummary.jsx
+++ src/components/CourseSummary.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Link from '@mui/material/Link'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
 
 const CourseSummary = (props) => {
-  return (
-    <section key={props.course.id} className="summary">
-        <div>
-            <div className='title'>
-                <h2>
-                    <Link className='no-underline cursor-pointer' to={`/courses/${props.course.slug}`}>
+    return (
+        <Card sx={{ maxWidth: 600, m: 'auto', mt: 2 }}>
+            <CardMedia
+                component="img"
+                height="140"
+                image="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210210175213/JavaScriptTutorial.png"
+            />
+            <CardContent >
+
+                <Typography gutterBottom variant="h5" component="div" fontFamily="serif" fontWeight="600">
+                    <Link href={`/courses/${props.course.slug}`} underline="none" color="inherit">
                         {props.course.title}
                     </Link>
-                </h2>
-            </div>
-            <p>
-            <Link className='no-underline cursor-pointer' to={`/courses/${props.course.slug}`}>
+                </Typography>
+
+                <Typography variant="h6" color="text.secondary" component="div" fontFamily="serif" >
+                    <Link href={`/courses/${props.course.slug}`} underline="none" color="inherit">
                         {props.course.description}
                     </Link>
-            </p>
-        </div>
+                </Typography>
+            </CardContent>
 
-    </section>
-  )
+        </Card>
+
+
+    )
 }
 
 export default CourseSummary
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import CourseSummary from '../components/CourseSummary'
 import courses from './courses'
+import Typography from '@mui/material/Typography';
 
 const Home = () => {
   return (
     <div className='home page'>
         <header>
             <h1>Online Courses</h1>
+            {/* <Typography align='center' component="h1" fontFamily="serif" fontSize="55" fontWeight="700">
+              Online Courses
+            </Typography> */}
         </header>
         {courses.map((course) => (
             <CourseSummary course={course} key={course.id}/>
