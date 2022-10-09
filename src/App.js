eabe0189b1eb1cc16127867ac5b2eeea2edd4ead
AlexKomanov <akomanov88@gmail.com>
2022-10-09 07:58:17 +0300

    Changing firebase storage with DynamoDB

--- src/App.js
+++ src/App.js
@@ -7,21 +7,56 @@ import Lesson from "./pages/Lesson";
 import { db } from './firebase-config';
 import { collection, getDocs} from 'firebase/firestore';
 import { useEffect, useState } from "react";
+import * as AWS from 'aws-sdk'
+
 
 const App = () => {
+  const docClient = new AWS.DynamoDB.DocumentClient()
+  const fetchData = (tableName) => {
+    let params = {
+        TableName: tableName
+    }
 
-  const [courses, setCourses] = useState([]);
+    docClient.scan(params, (err, data) => {
+            setCourses(data.Items)
+        
+    })
+    setLoading(false);
+}
+
+  const [courses, setCourses] = useState(['']);
+  const [loading, setLoading] = useState(true)
   const coursesCollection = collection(db, 'Courses')
 
+  // TODO - Check removing firestore query
+  // const getCourses = async () => {
+  //   const data = await getDocs(coursesCollection);
+  //   setCourses(data.docs.map((doc) => ({...doc.data()})));
+    
+  // };
+
   useEffect(() => {
-    const getCourses = async () => {
-      const data = await getDocs(coursesCollection);
-      setCourses(data.docs.map((doc) => ({...doc.data()})));
-      
-    };
-  
-    getCourses();
-  }, [courses])
+    
+    fetchData('courses');
+    // TODO - Check removing firestore query
+    // getCourses();
+  }, [])
+
+  if(loading) {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
+      >
+        Loading the data
+      </div>
+    )
+  }
 
 
   return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import * as AWS from 'aws-sdk'
+
+const configuration = {
+  region: process.env.REACT_APP_region,
+  secretAccessKey: process.env.REACT_APP_secretAccessKey,
+  accessKeyId: process.env.REACT_APP_accessKeyId
+
+}
+
+AWS.config.update(configuration);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
