2a8bcd670821d47630446030b639fe324ae91239
AlexKomanov <52656401+AlexKomanov@users.noreply.github.com>
2023-01-06 23:35:26 +0200

    Create AWSFunctions.js

---
+++ src/AWSFunctions.js
@@ -0,0 +1,24 @@
+import * as AWS from 'aws-sdk'
+
+const configuration = {
+    region: process.env.REACT_APP_region,
+    secretAccessKey: process.env.REACT_APP_secretAccessKey,
+    accessKeyId: process.env.REACT_APP_accessKeyId
+  
+  }
+  
+  AWS.config.update(configuration);
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+export const fetchData = async () => {
+    let params = {
+        TableName: 'courses'
+      }
+
+    let data = await docClient.scan(params).promise();
+    const items = data.Items.map((item) => {return item})
+    return items;
+    
+}
+    
